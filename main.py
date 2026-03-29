f43f73199c43dc339bdf7560ad75bffec28fe8fa
Carlos Bautista <115494128+CxLos@users.noreply.github.com>
2026-03-28 20:08:49 -0400

    fixed read_root test so that request is first argument and not second which was causing non-hashable error

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ async def read_root(request: Request):
     """
     Serve the index.html template.
     """
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse(request, "index.html")
 
 @app.get("/health")
 async def health_check():
